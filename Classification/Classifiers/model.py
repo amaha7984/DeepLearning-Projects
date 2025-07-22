a949ba22c0684057d5bf9067cb9daf64709ef9cd
Arpan Mahara <amaha038@fiu.edu>
2025-07-21 21:11:48 -0400

    Added ViT from Scratch Class

--- Classification/Classifiers/model.py
+++ Classification/Classifiers/model.py
@@ -19,6 +19,57 @@ class SimpleCNNClassifier(nn.Module):
         x = self.fc2(x)
         return x
 
+class ViTfromScratch(nn.Module):
+    def __init__(self, ch=3, img_size=224, patch_size=4, emb_dim=32,
+                n_layers=6, out_dim=53, dropout=0.1, heads=2): #out_dim=53, matching the number of classes
+        super(ViT, self).__init__()
+
+        # Attributes
+        self.channels = ch
+        self.height = img_size
+        self.width = img_size
+        self.patch_size = patch_size
+        self.n_layers = n_layers
+
+        # Patching
+        self.patch_embedding = PatchEmbedding(in_channels=ch,
+                                              patch_size=patch_size,
+                                              emb_size=emb_dim)
+        # Learnable params
+        num_patches = (img_size // patch_size) ** 2
+        self.pos_embedding = nn.Parameter(
+            torch.randn(1, num_patches + 1, emb_dim))
+        self.cls_token = nn.Parameter(torch.rand(1, 1, emb_dim))
+
+        # Transformer Encoder
+        self.layers = nn.ModuleList([])
+        for _ in range(n_layers):
+            transformer_block = nn.Sequential(
+                ResidualAdd(PreNorm(emb_dim, Attention(emb_dim, n_heads = heads, dropout = dropout))),
+                ResidualAdd(PreNorm(emb_dim, FeedForward(emb_dim, emb_dim, dropout = dropout))))
+            self.layers.append(transformer_block)
+
+        # Classification head
+        self.head = nn.Sequential(nn.LayerNorm(emb_dim), nn.Linear(emb_dim, out_dim))
+
+
+    def forward(self, img):
+        # Get patch embedding vectors
+        x = self.patch_embedding(img)
+        b, n, _ = x.shape
+
+        # Add cls token to inputs
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat([cls_tokens, x], dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+
+        # Transformer layers
+        for i in range(self.n_layers):
+            x = self.layers[i](x)
+
+        # Output based on classification token
+        return self.head(x[:, 0, :])
+
 class Mobilev3Classifier(nn.Module):
     def __init__(self, num_classes=53):
         super(Mobilev3Classifier, self).__init__()
@@ -40,7 +91,9 @@ class Mobilev3Classifier(nn.Module):
 MODEL_REGISTRY = {
     'simple_cnn': SimpleCNNClassifier,
     'mobilenetv3': Mobilev3Classifier,
+    'vit_scratch': ViTfromScratch,
 }
+
 
 def get_model(name, num_classes):
     assert name in MODEL_REGISTRY, f"Model '{name}' not found."
