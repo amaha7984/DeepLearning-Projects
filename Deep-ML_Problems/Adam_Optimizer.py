6423f0b4b8a69b685aa8b621fd23e3879607c85c
Arpan Mahara <amaha038@fiu.edu>
2025-07-31 11:24:22 -0400

    Adding correct PyTorch solution to Adam Optimizer

--- Deep-ML_Problems/Adam_Optimizer.py
+++ Deep-ML_Problems/Adam_Optimizer.py
@@ -6,17 +6,21 @@
 #PyTorch Solution
 import torch 
 
-def adam_optimizer(parameter, grad, m, v, t, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+def round_tensor(x): 
+    return torch.round(x * 1e5) / 1e5
 
+
+def adam_optimizer(parameter: torch.tensor, grad: torch.tensor, m: torch.Tensor, v: torch.Tensor, t: int, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+
+    grad = torch.tensor(grad, dtype=torch.float32)
     m_t = beta1 * m + (1-beta1) * grad
-    v_t = beta2 * v + (1-beta2) * grad**2
+    v_t = beta2 * v + (1-beta2) * grad.pow(2)
 
     m_bt = m_t/(1-beta1**t)
     v_bt = v_t/(1-beta2**t)
-    v_bt = torch.tensor(v_bt, dtype=torch.float32)
     
     parameter = parameter - learning_rate * (m_bt / ((torch.sqrt(v_bt)) + epsilon))
-    return np.round(parameter,5), np.round(m_t,5), np.round(v_t,5)
+    return round_tensor(parameter), round_tensor(m_t), round_tensor(v_t)
     
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
