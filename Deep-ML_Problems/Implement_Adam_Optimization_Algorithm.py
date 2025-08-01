9a848bf9cd3253ee0cbbf9e50052411d41ebcb03
Arpan Mahara <amaha038@fiu.edu>
2025-08-01 10:35:54 -0400

    Adding Numpy solution to Implement Adam Optimization Algorithm

--- Deep-ML_Problems/Implement_Adam_Optimization_Algorithm.py
+++ Deep-ML_Problems/Implement_Adam_Optimization_Algorithm.py
@@ -33,6 +33,38 @@ def adam_optimizer(f, grad, x0, learning_rate=0.001, beta1=0.9, beta2=0.999, eps
 
 
 
+#---------------------------------------------------------------------------------------------------------------------
+# Numpy Solution
+
+"""
+import numpy as np
+
+def objective_function(x):
+    return x[0]**2 + x[1]**2
+
+def gradient(x):
+    return np.array([2*x[0], 2*x[1]])
+
+
+def adam_optimizer(f, grad, x0, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, num_iterations=10):
+	x = x0.copy()
+	m = np.zeros_like(x)  
+	v = np.zeros_like(x)  
+
+	for i in range(num_iterations):
+
+		t = i + 1
+		g_t = grad(x)
+
+		m = beta1 * m + (1-beta1) * g_t
+		v = beta2 * v + (1-beta2) * (g_t ** 2)
+		
+		m_bt = m /(1-beta1 ** t)
+        v_bt = v /(1-beta2 ** t)
+
+		x = x - learning_rate * (m_bt / (np.sqrt(v_bt) + epsilon))
+	return x 
+"""
 
 
 
