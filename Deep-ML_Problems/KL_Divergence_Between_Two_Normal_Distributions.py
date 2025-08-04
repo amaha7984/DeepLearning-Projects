6da83da6f9cdd9c796e3c34c4bfe0debd9998e30
Arpan Mahara <amaha038@fiu.edu>
2025-08-03 20:20:04 -0400

    Adding PyTorch Solution to KL Divergence Between Two Normal Distribution

--- Deep-ML_Problems/KL_Divergence_Between_Two_Normal_Distributions.py
+++ Deep-ML_Problems/KL_Divergence_Between_Two_Normal_Distributions.py
@@ -1,8 +1,20 @@
 # Problem 56 : KL Divergence Between Two Normal Distributions
 # Task: Implement KL Divergence Between Two Normal Distributions
 # -----------------------------------------------------------------------------
-# Numpy Solution
+#PyTorch Solution
+import torch
+def kl_divergence_normal(mu_p, sigma_p, mu_q, sigma_q):
 
+	result = torch.log(torch.tensor(sigma_q / sigma_p)) + ((sigma_p**2 + (mu_p - mu_q)**2)/ (2 * sigma_q**2)) - 1/2
+ 
+	return result
+
+
+
+
+# -----------------------------------------------------------------------------
+# Numpy Solution
+"""
 import numpy as np
 
 def kl_divergence_normal(mu_p, sigma_p, mu_q, sigma_q):
@@ -10,3 +22,4 @@ def kl_divergence_normal(mu_p, sigma_p, mu_q, sigma_q):
 	result = np.log(sigma_q / sigma_p) + ((sigma_p**2 + (mu_p - mu_q)**2)/ (2 * sigma_q**2)) - 1/2
  
 	return result
+"""
