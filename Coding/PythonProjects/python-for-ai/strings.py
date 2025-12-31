8d7ffc6c6bf55c5f307811409d09c0f70b0875bc
Arpan Mahara <amaha038@fiu.edu>
2025-12-30 21:21:11 -0500

    Adding python file with strings manipulation information

---
+++ Coding/PythonProjects/python-for-ai/strings.py
@@ -0,0 +1,14 @@
+first_name = "Arpan"
+last_name = "Mahara"
+
+#concatentation
+full_name = first_name + " " + last_name
+print(full_name)
+
+#repetetion
+what = "?" * 5
+print(what)
+
+# string length
+#len()
+print(len(first_name))
