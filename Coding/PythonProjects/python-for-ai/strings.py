9096d58d8e5edfc669e5e8e0e254ceab6a97d1d6
Arpan Mahara <amaha038@fiu.edu>
2026-01-04 19:24:40 -0500

    Adding f-Strings usage

--- Coding/PythonProjects/python-for-ai/strings.py
+++ Coding/PythonProjects/python-for-ai/strings.py
@@ -2,7 +2,7 @@ first_name = "Arpan"
 last_name = "Mahara"
 
 #concatentation
-full_name = first_name + " " + last_name
+"""full_name = first_name + " " + last_name
 print(full_name)
 
 #repetetion
@@ -16,3 +16,17 @@ print(len(first_name))
 #Converting other types to string
 age = 28
 print("I am " + str(age) + " years old")
+
+"""
+
+#usage of f-Strings
+# Instead of: 
+print("My name is " + first_name + " " + last_name)
+# We can use:
+print(f"My name is {first_name} {last_name}")
+
+# Evaluate Expressions with f-Strings
+apples = 10
+orange = 5
+bananas = 10
+print(f"{first_name} has {apples + orange + bananas} fruits in total.")
