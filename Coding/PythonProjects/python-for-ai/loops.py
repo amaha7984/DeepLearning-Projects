f27f398ac1b0c9a23347580f56434976a0a2fbf4
Arpan Mahara <amaha038@fiu.edu>
2026-01-04 20:20:01 -0500

    Adding for loops usage

---
+++ Coding/PythonProjects/python-for-ai/loops.py
@@ -0,0 +1,9 @@
+# Count from 1 to 5
+for i in range(1, 6):
+    print(i)
+# Output: 1, 2, 3, 4, 5
+
+# Count by 2s
+for i in range(0, 10, 2): # We wa t to start from 0 and end at less than 10, and each interval is of 2
+    print(i)
+# Output: 0, 2, 4, 6, 8
