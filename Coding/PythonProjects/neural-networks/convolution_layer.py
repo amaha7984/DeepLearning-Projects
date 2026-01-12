6308ff32b9614e532d074d64bb356776f1eeec05
Arpan Mahara <amaha038@fiu.edu>
2026-01-11 21:36:39 -0500

    Adding 2D convolution layer

---
+++ Coding/PythonProjects/neural-networks/convolution_layer.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class conv2D:
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        self.stride = stride
+        self.padding = padding
+        self.kernel_size = kernel_size
+
+        self.weights = 0.1 * np.random.randn(
+            out_channels, in_channels, kernel_size
+        )
+        self.biases = np.zeros(out_channels)
+
+    def forward(self, X):
+        # X: (B, C, H, W)
+
+        batch_size, channels_in, H, W = X.shape
+        K = self.kernel_size
+        S = self.stride
+        p = self.padding
+
+        C_out = self.weights.shape[0]
+
+        if p > 0:
+            X = np.pad(X, ((0,0), (0,0), (p, p), (p, p)), mode = 'constant')
+        
+        # O = ((W - F + 2 * P)/S) + 1
+        output_height = ((X.shape[2] - K)//S) + 1
+        output_width = ((X.shape[3] - K)//S) + 1
+
+        output = np.zeros((batch_size, C_out, output_height, output_width))
+
+        for b in range(batch_size):
+            for c_out in range(C_out):
+                for i in range(output_height):
+                    for j in range(output_width):
+                        row = i * S
+                        col = j * S
+                        patch = X[b, :, row: row+K, col: col+K]
+                        output[b, c_out, i, j] = np.sum(np.multiply(patch, self.weight[c_out])) + self.biases[c_out]
+
+        self.output = output
+
+
+
