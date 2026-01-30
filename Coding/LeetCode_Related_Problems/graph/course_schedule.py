33a718a17db161d3623f7bd1e82d08a7b5b2f997
Arpan Mahara <amaha038@fiu.edu>
2026-01-30 10:46:56 -0500

    Updating solution by adding recursive DFS solution

--- Coding/LeetCode_Related_Problems/graph/course_schedule.py
+++ Coding/LeetCode_Related_Problems/graph/course_schedule.py
@@ -33,3 +33,55 @@ class Solution:
             if not dfs(i):
                 return False
         return True
+
+#----------------------------------------Iterative DFS (stack)-----------------------------------------#
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = defaultdict(list)
+        courses = prerequisites
+
+        for u, v in courses:
+            graph[u].append(v)
+        
+        not_visited = 0
+        visiting = 1
+        visited = 2
+
+        states = [0] * numCourses
+
+        stack = []
+
+        for node in range(numCourses):
+            if states[node] != not_visited: #skipping nodes that were already processed
+                continue
+
+            stack.append((node, 0))
+
+            while stack:
+                node, done = stack.pop()
+                if done == 1:
+                    states[node] = visited
+                    continue
+
+                state = states[node]
+                if state == visited:
+                    continue
+                elif state == visiting:
+                    return False
+                states[node] = visiting
+                stack.append((node, 1))
+    
+                for nei in graph[node]:
+                    state = states[nei]
+                    if state == visiting:
+                        return False
+                    if state == not_visited:
+                        stack.append((nei, 0))
+
+        return True
+                
+                
+
+
+
