0a93eec11bca1d706e4b65c884bea0c1fecc8604
Arpan Mahara <amaha038@fiu.edu>
2026-02-09 21:32:53 -0500

    Updating by adding simple 2d numpy implementation of CNN

--- Coding/PythonProjects/neural-networks/convolution_neural_networks/2D_cnn.py
+++ Coding/PythonProjects/neural-networks/convolution_neural_networks/2D_cnn.py
@@ -9,3 +9,33 @@ class ConvLayer(nn.Module):
         return self.conv(x)
    
 
+#---------------------numpy implementation-------------------------------------#
+
+# This is simple 2D convolution layer without channels and batch
+import numpy as np
+
+def simple_conv2d(input_matrix: np.ndarray, kernel: np.ndarray, padding: int, stride: int):
+
+    # O = (W - F + 2P)/S + 1
+
+    input_height, input_width = input_matrix.shape
+    kernel_height, kernel_width = kernel.shape
+
+    if padding > 0:
+        input_matrix = np.pad(input_matrix, ((padding, padding), (padding, padding)), mode='constant')
+    
+    output_height = ((input_height - kernel_height) // stride) + 1
+    output_width = ((input_width - kernel_width) // stride) + 1
+
+    output_matrix = np.zeros((output_height, output_width))
+
+    for i in range(0, output_height):
+        for j in range(0, output_width):
+            row = i * stride
+            col = j * stride
+            patch = input_matrix[row:row+kernel_height, col:col+kernel_width]
+            output_matrix[i, j]= np.sum(patch * kernel)
+    
+    return output_matrix
+
+
