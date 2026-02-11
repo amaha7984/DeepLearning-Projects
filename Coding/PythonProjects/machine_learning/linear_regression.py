1a423ee3ff32347b1c52e2000ab8b4c652fba1f7
Arpan Mahara <amaha038@fiu.edu>
2026-02-10 21:35:51 -0500

    Adding Linear Regression Definition

---
+++ Coding/PythonProjects/machine_learning/linear_regression.py
@@ -0,0 +1,10 @@
+"""
+Linear regression is a method for fitting a straight line (or hyperplane in higher dimensions) to given input features and their corresponding outputs,
+so that we can use this fitted line to predict the output for new feature values.
+
+# Definition
+
+Linear regression is a supervised learning method that models the relationship between one or more independent variables (features) and a dependent variable (target) 
+by assuming a linear relationship between them.
+
+"""
