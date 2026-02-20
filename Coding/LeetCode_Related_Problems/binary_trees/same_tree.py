03131652d7148052618b5c9702a4f9ba167caff1
Arpan Mahara <amaha038@fiu.edu>
2026-02-20 18:31:43 -0500

    Updating solution to Same Tree

--- Coding/LeetCode_Related_Problems/binary_trees/same_tree.py
+++ Coding/LeetCode_Related_Problems/binary_trees/same_tree.py
@@ -28,6 +28,22 @@ class Solution:
         
         return search(node1, node2)
 
+#-------------Another Solution----------#
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        if not p and not q:
+            return True
+        
+        elif not p or not q:
+            return False
+        
+        elif p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left, q.left) and 
+                self.isSameTree(p.right, q.right))
+
 
 
             
