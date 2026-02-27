fe3b7f29a0ad26c57ba4547e9c809959c7ea913a
Arpan Mahara <amaha038@fiu.edu>
2026-02-27 17:07:03 -0500

    Adding bfs solution to Question 111

--- Coding/LeetCode_Related_Problems/binary_trees/minimum_depth_of_binary_tree.py
+++ Coding/LeetCode_Related_Problems/binary_trees/minimum_depth_of_binary_tree.py
@@ -18,5 +18,29 @@ class Solution:
             return 1 + self.minDepth(root.left)
 
         return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+#-----------------------BFS Solution--------------------------------------#
+from collections import deque
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+
+        q = deque()
+        q.append((root, 1))
+
+        while q:
+            node, min_depth = q.popleft()
+
+            if not node.left and not node.right:
+                return min_depth
+            
+            if node.left:
+                q.append((node.left, min_depth + 1))
+            
+            if node.right:
+                q.append((node.right, min_depth + 1))
+
         
             
